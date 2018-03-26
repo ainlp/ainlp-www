c2d3db9e37ab68afb4602fed20dd8da8d5ed7d96
unamedkr <unamedkr@gmail.com>
2018-03-26 17:36:32 +0900

    first init

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <p> AI NLP KOREA </p>
+    <img src="./assets/think2.jpeg" width="80%">
+    <P> Who am I...</P>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
